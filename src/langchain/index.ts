204046c6f9b8c3c10480458b8cf08d854dd8aaa9
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-01-15 08:18:26 +0100

    - Fixed some rebasing errors, added missing package.json deps - refactored fluxbeam into folder - refactored tools - removed unused imports

--- src/langchain/index.ts
+++ src/langchain/index.ts
@@ -31,7 +31,7 @@ export * from "./voltr";
 export * from "./mayan";
 export * from "./allora";
 export * from "./switchboard";
-export * from "./fluxbeam"
+export * from "./fluxbeam";
 import type { SolanaAgentKit } from "../agent";
 import {
   SolanaBalanceTool,
@@ -142,6 +142,23 @@ import {
   SolanaAlloraGetAllTopics,
   SolanaAlloraGetInferenceByTopicId,
   SolanaSwitchboardSimulateFeed,
+  SolanaFluxbeamBridgeTokensTool,
+  SolanaFluxbeamBurnTokenTool,
+  SolanaFluxbeamCreatePoolTool,
+  SolanaFluxbeamSubmitFeeClaimTool,
+  SolanaFluxbeamSubmitFeePaymentTool,
+  SolanaFluxbeamUpdateV1MetadataTool,
+  SolanaFluxbeamUpdateV2MetadataTool,
+  SolanaFluxbeamMintToAccountTool,
+  SolanaFluxbeamSetAuthorityTool,
+  SolanaFluxbeamRevokeAuthorityTool,
+  SolanaFluxbeamCreateTokenV1Tool,
+  SolanaFluxbeamCreateTokenV2Tool,
+  SolanaFluxbeamGetClaimWithheldTokensFromMintTool,
+  SolanaFluxbeamGetClaimWithheldTokensToMintTool,
+  SolanaFluxbeamGetClaimWithheldTokensTool,
+  SolanaFluxbeamWrapSOLTool,
+  SolanaFluxbeamUnwrapSOLTool,
 } from "./index";
 
 export function createSolanaTools(solanaKit: SolanaAgentKit) {
@@ -260,13 +277,13 @@ export function createSolanaTools(solanaKit: SolanaAgentKit) {
     new SolanaFluxbeamCreatePoolTool(solanaKit),
     new SolanaFluxbeamSubmitFeeClaimTool(solanaKit),
     new SolanaFluxbeamSubmitFeePaymentTool(solanaKit),
-    new SolanaFluxbeamUpdateMetadataTool(solanaKit),
+    new SolanaFluxbeamUpdateV1MetadataTool(solanaKit),
     new SolanaFluxbeamUpdateV2MetadataTool(solanaKit),
     new SolanaFluxbeamMintToAccountTool(solanaKit),
     new SolanaFluxbeamSetAuthorityTool(solanaKit),
     new SolanaFluxbeamRevokeAuthorityTool(solanaKit),
-    new SolanaFluxbeamCreateMintV1Tool(solanaKit),
-    new SolanaFluxbeamCreateMintV2Tool(solanaKit),
+    new SolanaFluxbeamCreateTokenV1Tool(solanaKit),
+    new SolanaFluxbeamCreateTokenV2Tool(solanaKit),
     new SolanaFluxbeamGetClaimWithheldTokensFromMintTool(solanaKit),
     new SolanaFluxbeamGetClaimWithheldTokensToMintTool(solanaKit),
     new SolanaFluxbeamGetClaimWithheldTokensTool(solanaKit),
